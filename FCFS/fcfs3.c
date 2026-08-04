0fb6a0a3abef39e9afb06a99439adce9c465650f
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-04 06:29:22 +0530

    up

---
+++ FCFS/fcfs3.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+
+    int bt[n], wt[n], tat[n], ct[n], at[n];
+    float avgWT = 0, avgTAT = 0;
+
+    // Input Arrival Time and Burst Time
+    for(i = 0; i < n; i++) {
+        printf("\nProcess P%d\n", i + 1);
+
+        printf("Arrival Time: ");
+        scanf("%d", &at[i]);
+
+        printf("Burst Time: ");
+        scanf("%d", &bt[i]);
+    }
+
+    // FCFS Scheduling
+    ct[0] = at[0] + bt[0];
+    tat[0] = ct[0] - at[0];
+    wt[0] = tat[0] - bt[0];
+
+    for(i = 1; i < n; i++) {
+        // If CPU is idle
+        if(ct[i - 1] < at[i])
+            ct[i] = at[i] + bt[i];
+        else
+            ct[i] = ct[i - 1] + bt[i];
+
+        tat[i] = ct[i] - at[i];
+        wt[i] = tat[i] - bt[i];
+    }
+
+    // Display Result
+    printf("\n-------------------------------------------------------------\n");
+    printf("Process\tAT\tBT\tCT\tTAT\tWT\n");
+    printf("-------------------------------------------------------------\n");
+
+    for(i = 0; i < n; i++) {
+        printf("P%d\t%d\t%d\t%d\t%d\t%d\n",
+               i + 1, at[i], bt[i], ct[i], tat[i], wt[i]);
+
+        avgWT += wt[i];
+        avgTAT += tat[i];
+    }
+
+    printf("-------------------------------------------------------------\n");
+    printf("Average Waiting Time = %.2f\n", avgWT / n);
+    printf("Average Turnaround Time = %.2f\n", avgTAT / n);
+
+    return 0;
+}
